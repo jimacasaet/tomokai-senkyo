19b1be2a3071ceb2a4523f68a84c0d1acff879e3
unknown <jmerc87@gmail.com>
2015-05-09 19:46:41 +0800

    Removal of hello instances

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -3,16 +3,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
-
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'gettingstarted.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^', include('elect.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^elect/', include('elect.urls')),
 
 )
