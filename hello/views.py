f5421122e64cc6be5a8050eb6fb1c014eb8a2c2c
unknown <jmerc87@gmail.com>
2015-05-09 16:21:46 +0800

    More stupidity v2

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
+    return HttpResponse('Set me up.')
 
 
 def db(request):
