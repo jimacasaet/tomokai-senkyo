d5f618622efb63a2789cd028b1abe29bd9033918
unknown <jmerc87@gmail.com>
2015-05-09 15:42:43 +0800

    Revert "Added works"
    This reverts commit 887a4f6e8f480b1b43ae45ff94c5b27dc10edf0b.

--- works/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from works.models import Board, Activity
-
-class BoardAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('title',)}
-
-admin.site.register(Board,BoardAdmin)
-admin.site.register(Activity)
