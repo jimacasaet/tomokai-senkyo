887a4f6e8f480b1b43ae45ff94c5b27dc10edf0b
unknown <jmerc87@gmail.com>
2015-05-09 15:26:56 +0800

    Added works

---
+++ works/migrations/0001_initial.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Activity',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=140)),
+                ('progress', models.IntegerField(default=0)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Board',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(unique=True, max_length=140)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='activity',
+            name='board',
+            field=models.ForeignKey(to='works.Board'),
+        ),
+    ]
